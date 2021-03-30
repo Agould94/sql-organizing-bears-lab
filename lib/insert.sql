18a0c7027710dafabc2fa62cbf1557e23b25f3ec
Alexander Gould <alexander.gould94@gmail.com>
2021-03-30 14:26:33 -0700

    commit tree 88b7393e4c06049e9c74027c8757a4f7c12a735d

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, color, sex, temperament, alive) VALUES('Mr. Chocolate', 5, 'white','lazy','M','Y');
+INSERT INTO bears (name, age, color, sex, temperament, alive) VALUES('Rowdy', 3, 'blue','rowdy','F','Y');
+INSERT INTO bears (name, age, color, sex, temperament, alive) VALUES('Tabitha',105,'the color of terror','magical and wizened','F','unclear');
+INSERT INTO bears (name, age, color, sex, temperament, alive) VALUES('Sergant Brown', 12, 'brown', 'angry','M','N');
+INSERT INTO bears (name, age, color, sex, temperament, alive) VALUES('Melissa',1,'black','sweet','F','Y');
+INSERT INTO bears (name, age, color, sex, temperament, alive) VALUES('Grinch',50,'green','grouchy','M','Y');
+INSERT INTO bears (name, age, color, sex, temperament, alive) VALUES('Wendy',4,'pink','windy','F','Y');
+INSERT INTO bears (name, age, color, sex, temperament, alive) VALUES(NULL, 5,'translucent','killey','M','Y');
